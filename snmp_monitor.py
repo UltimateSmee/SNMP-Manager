0f38defa2e58d303cb1dd2b3b774248174cb9a48
Kaleo <iam1smee@gmail.com>
2024-01-23 10:07:58 -0500

    updated program for functionality and error handling
    changed around a lot of formatting with oid value display and error messages. GOOD GROUNDWORK to move on to the rest of the program.
    
    Need to figure out how to incorporate scheduling.
    
    Need to add function for sql database query.
    
    Need to add more oids and devices.

--- snmp_monitor.py
+++ snmp_monitor.py
@@ -1,10 +1,10 @@
 import json
 import schedule
 import time
+# time for scheduling? 
 import sqlite3
 import sys
 from pysnmp.hlapi import *
-
 
 
 def get_snmp_data(ip, community, oid):
@@ -31,33 +31,45 @@ def get_snmp_data(ip, community, oid):
 			CommunityData(community),
 			UdpTransportTarget((ip, 161)),
 			ContextData(),
-			ObjectType(ObjectIdentity(oid)
-		)))
+			ObjectType(ObjectIdentity(oid))
+			))
 
 		if errorIndication:
-			print(f"Error: {errorIndication}")
+			print(f"Error Indication: {errorIndication}")
+			return
 		elif errorStatus:
-			print(f"Error: {errorStatus}")
+			print(f"Error Status: {errorStatus}")
+			return
 		else:
 			if not varBinds:
 				print(f"No values returned for OID {oid}")
 			else:
 				for varBind in varBinds:
-					print(f"{varBind[0]} = {varBind[1]}")
+					# Cases where it returns nothing, format the output to be more readable
+					if not varBind[1]:
+						print(f"{str(varBind)[str(varBind).rfind('No Such'):]}: {varBind[0]} ( {str(varBind)[12:str(varBind).rfind('No Such')-2]} )")
+					else:
+						print(f"{varBind[0]} = {varBind[1]}")
 
-		cursor.execute('''
-			INSERT INTO snmp_data(ip_address, community, oid, value)
-			VALUES(?, ?, ?, ?)
-		''', (ip, community, oid, varBind[1].prettyPrint()))
+		cursor.execute('''INSERT INTO snmp_data(ip_address, community, oid, value) VALUES(?, ?, ?, ?)''', (ip, community, oid, varBind[1].prettyPrint()))
 
 	except Exception as e:
-		print(f"Error during SNMP query: {str(e)}")
+		# Common errors with error numbers will be displayed rather than the whole output
+		t = str(e).rfind('[Errno')
+		if t != -1:
+			e = str(e)[t:]
+		print(f"""
+	#
+	Error during SNMP query: 
+	Device: {ip}
+	Error: {str(e)}
+	#
+			""")
 
 # Commit Changes and close connection
 
 	conn.commit()
 	conn.close()
-
 
 
 def add_device(ip, community, oids=[]):
@@ -76,25 +88,27 @@ def add_device(ip, community, oids=[]):
 			print(f"Device with IP {ip} already exists.")
 
 # If more than one oid, append to ip
-			for arg in oids:
-				if arg in device:
-					print(f"Oid {arg} for device with ip {ip} already exists")
+			o = len(device["oids"]) - 1
+			for oid in oids:
+				if oid in device["oids"]:
+					print(f"Oid {oid} for device with ip {ip} already exists")
 					continue
 
-				new_oid = {"oid"+arg: arg}
-				print(devices)
-				devices[d].append(new_oid)
+				device["oids"].append(oid)
+				print(f"Oid, {oid} added to device with ip {ip}")
+				o+=1
 
 			with open('devices.json', 'w') as file:
 				json.dump(devices, file, indent=2)
-			print(f"Device oid {arg} added to ip {ip}")
+			
 			return
+		d+=1
+			
 
 
+# Continue to add device to list if it doesnt exist
 
-# Continue to add device to list 
-
-	new_device = {"ip": ip, "community": community, "oid": oids[0]}
+	new_device = {"ip": ip, "community": community, "oids": oids}
 	devices.append(new_device)
 
 	with open('devices.json', 'w') as file:
@@ -105,52 +119,51 @@ def add_device(ip, community, oids=[]):
 
 
 def job():
-
+# ADD ABILITY TO RUN SELECTED DEVICES. CHECK FOR DEVICE, IF NOT FOUND, ASK IF WANT TO ADD THEN RUN FOR THAT DEVICE
+# ADD RUN ALL VS RUN ONE
 # Load devices from devices.json
 	try:
 		with open('devices.json', 'r') as file:
 			devices = json.load(file)
-	except FileNotFouandError:
+	except FileNotFoundError:
 		devices = []
 
 # Check for devices to query
-	if not Devices:
+	if not devices:
 		print("No devices to query.")
 		return
 
 
 # Query each device
 	for device in devices:
-		ip, community, oid = device["ip"], device["community"], device["oid"]
-		get_snmp_data(ip, community, oid)
-
+		ip, community = device["ip"], device["community"]
+		for oid in device["oids"]:
+			get_snmp_data(ip, community, oid)
 
 schedule.every(5).seconds.do(job)
+	
 
 
 if __name__ == "__main__":
-	if (len(sys.argv) != 4 and ("add" not in sys.argv)):
+	if "run" in sys.argv:
+		job()
+		
+	elif "add" in sys.argv:
+		ip, community = sys.argv[2], sys.argv[3]
+		oids=[]
+		for o in range(len(sys.argv)-4):
+			oids.append(sys.argv[4+o])
+		add_device(ip, community, oids)
+	
+	elif len(sys.argv) == 4:
+		ip, community, oid = sys.argv[1], sys.argv[2], sys.argv[3]
+		get_snmp_data(ip, community, oid)
+
+	else:
 		print(f"""Usage:
 			To run query on all devices in devices.json: python3 snmp_monitor.py 
 			To run query on device: python3 snmp_monitor.py <ip> <community> <oid> 
 			To add a new device: python3 snmp_monitor.py add <ip> <community> <oids...>
 			""")
 		sys.exit(1)
-	elif len(sys.argv) == 1:
-		job()
-
-	elif "add" in sys.argv:
-		ip, community = sys.argv[2], sys.argv[3]
-		oids=[]
-		for o in range(len(sys.argv)-4):
-			oids.append(sys.argv[4+o])
-		print(oids)
-		add_device(ip, community, oids)
-
-
-	ip, community, oid = sys.argv[1], sys.argv[2], sys.argv[3]
-	get_snmp_data(ip, community, oid)
-
-
-# get_snmp_data('192.168.1.1', 'public', '1.3.6.1.2.1.2.2.1.10.1')
 
