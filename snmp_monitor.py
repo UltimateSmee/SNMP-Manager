c5c76783158979383e575f4f4b097ac8626f1dd0
Kaleo <iam1smee@gmail.com>
2024-01-29 14:30:18 -0500

    added ping function and snmpdump
    changed to use snmp dump rather than get to speed up query. also able to report multiple oids at a time.
    
    need to do:
    add function to query network for available devices
    
    be able to determine subnet user input

--- snmp_monitor.py
+++ snmp_monitor.py
@@ -5,6 +5,43 @@ import time
 import sqlite3
 import sys
 from pysnmp.hlapi import *
+from pysnmp import debug
+from ping3 import ping
+
+def network_discovery(ips):
+	# Ping all IP's on network
+	return
+
+def ping_device(ip):
+	# Ping device, return true if it is reachable
+	try:
+		if ping(ip, timeout=4):
+			print(f"Device at {ip} IS reachable.")
+			return True
+		
+	except Exception as e:
+		print(f"Error during Ping: {str(e)}")
+
+	print(f"Device at {ip} IS NOT reachable.")
+	return False
+
+def check_snmp(ip):
+	try:
+		getCmd(
+			SnmpEngine(),
+			CommunityData("public"),
+			UdpTransportTarget((ip, 161)),
+			ContextData(),
+			ObjectType(ObjectIdentity("1.3.6.1.2.1.1.5.0"))
+		)
+		print(f"{ip} is available for SNMP query")
+		return True
+	
+	except Exception as e:
+		print(str(e))
+		print(f"Unable to query SNMP on {ip}")
+
+	return False
 
 def query_database():
 	# Connect to the database and create cursor
@@ -36,7 +73,6 @@ def get_snmp_data(ip, community, oid):
 	cursor = conn.cursor()
 
 # Create a table to store SNMP data
-
 	cursor.execute('''
 		CREATE TABLE IF NOT EXISTS snmp_data (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -47,33 +83,43 @@ def get_snmp_data(ip, community, oid):
 			value STRING
 		)
 	''')
-	try: 
-		errorIndication, errorStatus, errorIndex, varBinds = next(getCmd(
+
+# Maximum amount of iterations for snmpwalk (nextCmd()) command
+	mx = 100
+	iterator = bulkCmd(
 			SnmpEngine(),
 			CommunityData(community),
 			UdpTransportTarget((ip, 161)),
 			ContextData(),
-			ObjectType(ObjectIdentity(oid))
-			))
-
-		if errorIndication:
-			print(f"Error Indication: {errorIndication}")
-			return
-		elif errorStatus:
-			print(f"Error Status: {errorStatus}")
-			return
-		else:
-			if not varBinds:
-				print(f"No values returned for OID {oid}")
+			0, 25,
+			ObjectType(ObjectIdentity(oid)),
+			lexicographicMode=False
+		)
+	try:
+		for i in range(mx):
+			errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+			
+			if errorIndication or errorStatus:
+				print(f"Error Indication: {errorIndication} {errorStatus}")
+				return
+			
 			else:
-				for varBind in varBinds:
-					# Cases where it returns nothing, format the output to be more readable
-					if not varBind[1]:
-						print(f"{str(varBind)[str(varBind).rfind('No Such'):]}: {varBind[0]} ( {str(varBind)[12:str(varBind).rfind('No Such')-2]} )")
-					else:
-						print(f"{varBind[0]} = {varBind[1]}")
+				if not varBinds:
+					print(f"No values returned for OID {oid}")
+				else:
+					for varBind in varBinds:
+						# Cases where it returns nothing, format the output to be more readable
+						if not varBinds:
+							print(f"{str(varBind)[str(varBind).rfind('No Such'):]}: {varBind[0]} ( {str(varBind)[12:str(varBind).rfind('No Such')-2]} )")
+						else:
+							print(f"{varBind[0]} = {varBind[1]}")
 
-		cursor.execute('''INSERT INTO snmp_data(ip_address, community, oid, value) VALUES(?, ?, ?, ?)''', (ip, community, oid, varBind[1].prettyPrint()))
+						
+			cursor.execute('''INSERT INTO snmp_data(ip_address, community, oid, value) VALUES(?, ?, ?, ?)''', (ip, community, oid, varBind[1].prettyPrint()))
+			i+=1
+
+	except StopIteration:
+		return
 
 	except Exception as e:
 		# Common errors with error numbers will be displayed rather than the whole output
@@ -160,9 +206,21 @@ schedule.every(5).seconds.do(job)
 	
 
 if __name__ == "__main__":
+
+	usage = """Usage:
+python3 snmp_monitor.py [options] [[IP's], [IP, Community, OID], [OID's]]...
+
+	ping	To check device, or networks', availability:	python3 snmp_monitor.py PING <<ip> OR <ip/CIDR> OR <ip AND NETMASK>>
+	query	To run query on all devices in devices.json:	python3 snmp_monitor.py QUERY
+	run 	To run snmp query on device: 	python3 snmp_monitor.py <ip> <community> <oid> 
+	add 	To add a new device:	python3 snmp_monitor.py ADD <ip> <community> <oids...>
+	"""
+
 	# Store length of command line variables to be checked later
 	l = len(sys.argv)
-
+	##############################################
+	#debug.setLogger(debug.Debug('all'))
+	##############################################
 	# Determine which funtion to run based on command line arguments
 	if l == 2:
 		if "run" in sys.argv:
@@ -170,23 +228,31 @@ if __name__ == "__main__":
 			
 		elif "query" in sys.argv:
 			query_database()
-
-		elif "add" in sys.argv:
-			ip, community = sys.argv[2], sys.argv[3]
-			oids=[]
-			for o in range(len(sys.argv)-4):
-				oids.append(sys.argv[4+o])
-			add_device(ip, community, oids)
+		else:
+			print(usage)
 		
+	elif l == 3:	
+		if "ping" in sys.argv:
+			for i in range(l-2):
+				# Ping device for connectivity
+				ip = sys.argv[i-1]
+				if ping_device(ip):
+					check_snmp(ip)
+		else:
+			print(usage)
+
 	elif l == 4:
 		ip, community, oid = sys.argv[1], sys.argv[2], sys.argv[3]
 		get_snmp_data(ip, community, oid)
 
+	elif "add" in sys.argv:
+			ip, community = sys.argv[2], sys.argv[3]
+			oids=[]
+			for o in range(l-4):
+				oids.append(sys.argv[4+o])
+			add_device(ip, community, oids)
+
 	else:
-		print(f"""Usage:
-			To run query on all devices in devices.json: python3 snmp_monitor.py 
-			To run query on device: python3 snmp_monitor.py <ip> <community> <oid> 
-			To add a new device: python3 snmp_monitor.py add <ip> <community> <oids...>
-			""")
+		print(usage)
 		sys.exit(1)
 
